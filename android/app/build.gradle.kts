ffe63046b044e60291ef028834e39784bdf94914
unclip12 <unclip-kite-stooge@duck.com>
2026-03-11 18:21:07 +0530

    fix: ci build failures
    - Update compileSdk to 36 in build.gradle.kts (required by media_kit)
    - Pin pdfrx to 1.0.77 (1.0.94 is incompatible with Flutter 3.29.0)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.focusflow.focusflow_mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
