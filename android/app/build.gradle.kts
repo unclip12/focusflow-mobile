ecbaa2a10542c09b287a60c68f3e3df09cea744d
unclip12 <unclip-kite-stooge@duck.com>
2026-04-01 08:24:02 +0530

    Raise Android minSdk to 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.focusflow.focusflow_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
